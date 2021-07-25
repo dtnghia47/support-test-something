ea5af8701e434d5e4744ec157dc7b599e5fd9f03
dtnghia47 <dtnghia47@gmail.com>
2021-07-25 23:21:11 +0700

    update tesseract

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
-import quickstart from "./utils";
+import Tesseract, { createWorker } from "tesseract.js";
 
 function App() {
+  const [textOcr, setText] = useState("");
+  const [loading, setLoading] = useState(false);
+  const hadnlequickstart = async (path: string) => {
+    try {
+      console.log("###start");
+      setLoading(true);
+      const { createWorker } = require("tesseract.js");
+
+      const worker = createWorker();
+      await worker.load();
+      await worker.loadLanguage("eng");
+      await worker.initialize("eng");
+      const {
+        data: { text },
+      } = await worker.recognize(path);
+      console.log("####terminate: ", text);
+      setText(() => {
+        setLoading(false);
+        return text;
+      });
+      await worker.terminate();
+    } catch (err) {
+      console.log(err);
+      setText("");
+      setLoading(false);
+    }
+  };
+  console.log("###loading:", loading);
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={() => quickstart()}>Demo button</button>
+        <div
+          className="row"
+          style={{ marginTop: "50px", marginBottom: "50px" }}
+        >
+          <a
+            href="https://i.imgur.com/s5dvxXD.jpeg"
+            target="_blank"
+          >
+            <img src="https://i.imgur.com/s5dvxXD.jpeg" className="text-img" />
+          </a>
+          <button
+            onClick={() => hadnlequickstart("https://i.imgur.com/s5dvxXD.jpeg")}
+          >
+            check external image
+          </button>
+        </div>
+        <div
+          className="row"
+          style={{ marginBottom: "50px" }}
+        >
+          <a
+            href="/assets/demo.png"
+            target="_blank"
+          >
+            <img src="/assets/demo.png" className="text-img" />
+          </a>
+          <button
+            onClick={() => hadnlequickstart("/assets/demo.png")}
+          >
+            check local image
+          </button>
+        </div>
+        {loading && <h2 className="loading">Loading .......!!</h2>}
+        <textarea value={textOcr}></textarea>
       </header>
     </div>
   );
